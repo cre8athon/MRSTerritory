c9fe47586fa004ebaeb17ec95f662d28a1285bdb
Nowakowski <gnowakow@amazon.com>
2016-11-01 15:52:10 -0400

    Adding javascript

---
+++ js/streetAliasJSON_1.0.0.js
@@ -0,0 +1,1 @@
+var streetAliasJSON = '{"street_alias":{"wash_valley":"washington_valley_rd", "wash_val":"washington_valley_rd","wash_valley":"washington_valley_rd","w_valley":"washington_valley_rd","washington_valley_rd":"washington_valley_rd"},"group_homes":{"by_name":{"advoserve":true,"badvoserve":true},"by_address":{"1234343":"11 foothill rd","2223332":"263 washington valley rd"}}}';
