b681c71fa7a33c0ba6b85c5efe3622037e7be164
Nowakowski <gnowakow@amazon.com>
2017-08-02 21:27:21 -0400

    Release on 20170802

--- js/MRSCallTerritory.js
+++ js/MRSCallTerritory.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         MRS Call Territory
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.2.1
 // @description  try to take over the world!
 // @author       You
 // @match        *://www.iamresponding.com/v3/*
